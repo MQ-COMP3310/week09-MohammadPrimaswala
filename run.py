f0979338f590cb99be998b9d41f01d94ef341c40
MohammadPrimaswala <mohammadprimuswala@gmail.com>
2025-05-06 13:54:08 +1000

    updated run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from project import create_app
+import os
 
 if __name__ == '__main__':
   app = create_app()
-  app.run(host = '0.0.0.0', port = 8001, debug=false) 
-
+  debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 't']
+  app.run(host = '0.0.0.0', port = 8001, debug=debug_mode) 
